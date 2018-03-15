28bb9cec8f59d04eff74c49acb2c1a41d514df42
HW-Lee <tony123930@gmail.com>
2018-03-15 17:54:35 +0800

    modified test tools

--- tools-for-dev/viewclient-example.py
+++ tools-for-dev/viewclient-example.py
@@ -9,13 +9,21 @@ def main():
     for key, value in views_dict.items():
         print("{}:\n{}\n".format(key, unicode(value).encode("UTF-8")))
 
-    views = views_dict.values()
-    id_view_dict = dict(map(lambda v: (v.getId(), v), [v for v in views if len(v.getId()) > 0]))
+    views = filter(lambda v: len(v.getId()) > 0, views_dict.values())
+    id_view_dict = {}
+    for v in views:
+        if v.getId() in id_view_dict.keys():
+            id_view_dict[v.getId()].append(v)
+        else:
+            id_view_dict[v.getId()] = [v]
 
     print("\n")
     print("-------- Print the id-to-view pairs --------")
     for key, value in id_view_dict.items():
-        print("{}:\n{}\n".format(key, unicode(value).encode("UTF-8")))
+        for each in value:
+            print("{}:\n{}\n".format(key, unicode(each).encode("UTF-8")))
+
+    vc.traverse()
 
     pass    
 
