228b792d11c5219844a1c394af0f282b1daa3664
HW-Lee <tony123930@gmail.com>
2018-03-26 14:22:13 +0800

    update genwave.py
    - add help command
    - use pcm16 as the encoding format rather than floating

--- tools-for-dev/genwave.py
+++ tools-for-dev/genwave.py
@@ -1,22 +1,26 @@
-from librosa.output import write_wav as wavwrite
+from scipy.io.wavfile import write as wavwrite
 import numpy as np
 import sys
 
 
 def main(argv):
+    if str(argv[0]) == "help":
+        print("python genwave.py FREQ DURATION FS NUMCH OUTPATH")
+        return
+
     freq = float(argv[0]) if len(argv) > 0 else 440
     duration = float(argv[1]) if len(argv) > 1 else 30
     fs = int(argv[2]) if len(argv) > 2 else 44100
     nch = int(argv[3]) if len(argv) > 3 else 2
     outpath = str(argv[4]) if len(argv) > 4 else "out.wav"
 
-    sig = np.zeros([int(fs*duration), nch])
+    sig = np.zeros([int(fs*duration), nch], dtype=np.int16)
     sinewave = 0.99 * np.sin(np.arange(sig.shape[0]) * freq*1.0/fs * 2*np.pi)
 
     for ich in range(sig.shape[1]):
-        sig[:, ich] = sinewave
+        sig[:, ich] = np.round(sinewave * (2**15-1))
 
-    wavwrite(outpath, sig, fs)
+    wavwrite(outpath, fs, sig)
 
 
 if __name__ == "__main__":
