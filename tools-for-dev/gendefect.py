17d3635536112bece19f2937597563ee8417d15e
HW-Lee <tony123930@gmail.com>
2018-03-29 17:32:29 +0800

    refactor the tools

--- tools-for-dev/gendefect.py
+++ tools-for-dev/gendefect.py
@@ -8,6 +8,7 @@ def main(argv):
     if str(argv[0]) == "help" or len(argv) < 1:
         print("python gendefect.py glitch INPATH OUTPATH FRAMESIZE(MS) DURATION(MS)")
         print("python gendefect.py nosound INPATH OUTPATH OFFSET(MS) DURATION(MS)")
+        print("python gendefect.py intermitted INPATH OUTPATH FRAMESIZE(MS) DURATION(MS)")
         return
 
     defect_name = str(argv[0]).lower()
@@ -16,18 +17,31 @@ def main(argv):
     params = []
 
     sig, fs = audioload(inpath, sr=None, mono=False)
-    sig = np.array(np.round(sig * (2**15-1)), dtype=np.int16)
+    sig = np.array(sig * (2**15-1))
     sig = sig.transpose()
 
-    if defect_name == "glitch":
+    if defect_name == "glitch" or defect_name == "intermitted":
         params.append(float(argv[3]) if len(argv) > 3 else 1)
         params.append(float(argv[4]) if len(argv) > 4 else 20)
         framesize = int(round(params[0]/1000.*fs))
         duration = int(round(params[1]/1000.*fs))
 
         for offset in range(0, sig.shape[0]-framesize, duration):
-            if sig.ndim > 1: sig[offset:offset+framesize, :] = 0
-            else: sig[offset:offset+framesize] = 0
+            if defect_name == "intermitted":
+                ramp_mask = np.arange(0., 1., 1./(framesize/2.))
+                start_idx = max([offset-framesize/2, 0])
+                end_idx = min([offset+framesize+framesize/2, sig.shape[0]])
+            if sig.ndim > 1:
+                sig[offset:offset+framesize, :] = 0
+                if defect_name == "intermitted":
+                    for ch in range(sig.shape[1]):
+                        sig[start_idx:offset+1, ch] *= ramp_mask[::-1][-(offset-start_idx+1):]
+                        sig[offset+framesize:end_idx, ch] *= ramp_mask[:(end_idx-offset-framesize)]
+            else:
+                sig[offset:offset+framesize] = 0
+                if defect_name == "intermitted":
+                    sig[start_idx:offset+1] *= ramp_mask[::-1][-(offset-start_idx+1):]
+                    sig[offset+framesize:end_idx] *= ramp_mask[:(end_idx-offset-framesize)]
 
     elif defect_name == "nosound":
         params.append(float(argv[3]) if len(argv) > 3 else 0)
@@ -42,6 +56,7 @@ def main(argv):
         print("unknown defect name.")
         return
 
+    sig = np.array(np.round(sig), dtype=np.int16)
     wavwrite(outpath, fs, sig)
 
 
--- tools-for-dev/visaudio.py
+++ tools-for-dev/visaudio.py
@@ -3,6 +3,36 @@ import numpy as np
 import matplotlib.pyplot as plt
 import sys
 
+def print_sig(signal, fs, outpath, horiz_extent_ratio, tfrom, tto):
+    num_ch = signal.shape[0] if signal.ndim == 2 else 1
+    num_sample = signal.transpose().shape[0]
+    duration = float(num_sample*1./fs)
+    t_axis = np.arange(0, duration, duration/(num_sample+1))
+    t_axis = t_axis[:num_sample]
+    
+    if num_ch > 1:
+        for i, ch_sig in enumerate(signal):
+            plt.subplot(num_ch, 1, i+1)
+            plt.plot(t_axis, ch_sig)
+            plt.gca().set_xlabel("time (s)")
+            if tfrom != None and tto != None:
+                plt.xlim([tfrom, tto])
+            xlim = plt.gca().get_xlim()
+            plt.gcf().set_size_inches([(xlim[1]-xlim[0])*horiz_extent_ratio/num_ch, 3])
+    else:
+        plt.plot(t_axis, signal)
+        plt.gca().set_xlabel("time (s)")
+        if tfrom != None and tto != None:
+            plt.xlim([tfrom, tto])
+        xlim = plt.gca().get_xlim()
+        plt.gcf().set_size_inches([(xlim[1]-xlim[0])*horiz_extent_ratio, 3])
+
+    try:
+        plt.savefig(outpath, bbox_inches="tight", pad_inches=0, dpi=300)
+    except Exception as e:
+        plt.savefig(outpath, bbox_inches="tight", pad_inches=0)
+
+    plt.gcf().clear()
 
 def main(argv):
     if str(argv[0]) == "help" or len(argv) < 5:
@@ -16,35 +46,7 @@ def main(argv):
     tto = float(argv[4])
 
     sig, fs = audioload(filepath, sr=None, mono=False)
-    num_ch = sig.shape[0] if sig.ndim == 2 else 1
-    num_sample = sig.transpose().shape[0]
-    duration = float(num_sample*1./fs)
-    t_axis = np.arange(0, duration, duration/(num_sample+1))
-    t_axis = t_axis[:num_sample]
-    
-    if num_ch > 1:
-        for i, ch_sig in enumerate(sig):
-            plt.subplot(num_ch, 1, i+1)
-            plt.plot(t_axis, ch_sig)
-            plt.gca().set_xlabel("time (s)")
-            if tfrom != None and tto != None:
-                plt.xlim([tfrom, tto])
-            xlim = plt.gca().get_xlim()
-            plt.gcf().set_size_inches([(xlim[1]-xlim[0])*horiz_extent_ratio/num_ch, 3])
-    else:
-        plt.plot(t_axis, sig)
-        plt.gca().set_xlabel("time (s)")
-        if tfrom != None and tto != None:
-            plt.xlim([tfrom, tto])
-        xlim = plt.gca().get_xlim()
-        plt.gcf().set_size_inches([(xlim[1]-xlim[0])*horiz_extent_ratio, 3])
-
-    try:
-        plt.savefig(outpath, bbox_inches="tight", pad_inches=0, dpi=300)
-    except Exception as e:
-        plt.savefig(outpath, bbox_inches="tight", pad_inches=0)
-
-    plt.gcf().clear()
+    print_sig(signal=sig, fs=fs, outpath=outpath, horiz_extent_ratio=horiz_extent_ratio, tfrom=tfrom, tto=tto)
 
 if __name__ == "__main__":
     main(sys.argv[1:])
