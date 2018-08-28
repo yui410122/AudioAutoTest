960cd9b1e816a3ba602021fd4d419f164005cee7
HW-Lee <tony123930@gmail.com>
2018-08-28 16:38:15 +0800

    add amplitude params

--- tools-for-dev/genwave.py
+++ tools-for-dev/genwave.py
@@ -5,17 +5,18 @@ import sys
 
 def main(argv):
     if str(argv[0]) == "help":
-        print("python genwave.py FREQ DURATION FS NUMCH OUTPATH")
+        print("python genwave.py FREQ AMP DURATION FS NUMCH OUTPATH")
         return
 
     freq = float(argv[0]) if len(argv) > 0 else 440
-    duration = float(argv[1]) if len(argv) > 1 else 30
-    fs = int(argv[2]) if len(argv) > 2 else 44100
-    nch = int(argv[3]) if len(argv) > 3 else 2
-    outpath = str(argv[4]) if len(argv) > 4 else "out.wav"
+    amp = float(argv[1]) if len(argv) > 1 else 0.7
+    duration = float(argv[2]) if len(argv) > 2 else 30
+    fs = int(argv[3]) if len(argv) > 3 else 44100
+    nch = int(argv[4]) if len(argv) > 4 else 2
+    outpath = str(argv[5]) if len(argv) > 5 else "out.wav"
 
     sig = np.zeros([int(fs*duration), nch], dtype=np.int16)
-    sinewave = 0.99 * np.sin(np.arange(sig.shape[0]) * freq*1.0/fs * 2*np.pi)
+    sinewave = amp * np.sin(np.arange(sig.shape[0]) * freq*1.0/fs * 2*np.pi)
 
     for ich in range(sig.shape[1]):
         sig[:, ich] = np.round(sinewave * (2**15-1))
